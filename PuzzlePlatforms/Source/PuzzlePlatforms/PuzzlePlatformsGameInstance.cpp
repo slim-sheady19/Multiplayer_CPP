e491701537145a412fe1d5ec9c3525db61a2b127
Shea <galley.shea@gmail.com>
2022-01-26 18:04:55 -0500

    18 Console commands with Exec keyword + 19 Hosting Servers with ServerTravel

---
+++ PuzzlePlatforms/Source/PuzzlePlatforms/PuzzlePlatformsGameInstance.cpp
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PuzzlePlatformsGameInstance.h"
+
+//constructor is called in editor
+UPuzzlePlatformsGameInstance::UPuzzlePlatformsGameInstance(const FObjectInitializer& ObjectInitializer)
+{
+	UE_LOG(LogTemp, Warning, TEXT("GameInstance Constructor"));
+}
+
+//init gets called at begin play
+void UPuzzlePlatformsGameInstance::Init()
+{
+	UE_LOG(LogTemp, Warning, TEXT("GameInstance Init"));
+}
+
+void UPuzzlePlatformsGameInstance::Host()
+{
+	UEngine* Engine = GetEngine(); //GetEngine is a method on the GI, returns Engine object
+	if (!ensure(Engine != nullptr)) return; //is valid check
+
+	Engine->AddOnScreenDebugMessage(0, 2, FColor::Green, TEXT("Hosting"));
+
+	UWorld* World = GetWorld(); //Get world object that has ServerTravel method on it
+	if (!ensure(World != nullptr)) return;
+
+	//ServerTravel		https://docs.unrealengine.com/4.27/en-US/API/Runtime/Engine/Engine/UWorld/ServerTravel/
+	World->ServerTravel("/Game/ThirdPersonCPP/Maps/ThirdPersonExampleMap?listen"); //?listen ensures it is a listenserver which allows others to connect
+}
+
+void UPuzzlePlatformsGameInstance::Join(const FString& Address)
+{
+	UEngine* Engine = GetEngine();
+	if (!ensure(Engine != nullptr)) return;
+
+	Engine->AddOnScreenDebugMessage(0, 5, FColor::Green, FString::Printf(TEXT("Joining %s"), *Address));
+}
+
